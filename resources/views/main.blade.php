0f5e85a805aa5ec1158eca91cbb74f0c91dfff9e
Bayu Rifki Alghifari <bayurifkialgh@gmail.com>
2021-12-20 15:57:49 +0700

    add new router to web.php and vue-router pakage

---
+++ resources/views/main.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Laravel Vue</title>
+    <style>
+        body {
+            font-family: sans-serif;
+            margin: 15%;
+        }
+    </style>
+</head>
+<body>
+    
+    <div id="app">
+
+    </div>
+    
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
